48812e8a32c9f348728ca2ed158f7f334722037d
JAghazada <jamilaghazada03@gmail.com>
2023-10-03 15:46:52 +0400

    updated

--- controllers/page.controllers.js
+++ controllers/page.controllers.js
@@ -2,11 +2,12 @@ import { UserModel } from "../db/UserModel.js";
 
 export const Home_PageController = async(req,res)=>{
     const users = await UserModel.find().lean();
-    // res.render("index",{users});
-    res.json({users})
+    res.render("index",{users});
+    // res.json({users})
 };
 
 
 export const Admin_PageController =(req,res)=>{
-    res.json({"admin":"admin"});
+    // res.json({"admin":"admin"});
+    res.render("admin");
 }
